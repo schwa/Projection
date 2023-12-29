a5bf9d9b7967c13aa35c42699f03cac88824dad9
Jonathan Wight <schwa@schwa.io>
2023-12-29 10:25:09 -0800

    Rekit normals.

--- Demos/Packages/GeometryX/Sources/GeometryX/TrivialMesh/TrivialMesh+RealityKit.swift
+++ Demos/Packages/GeometryX/Sources/GeometryX/TrivialMesh/TrivialMesh+RealityKit.swift
@@ -4,11 +4,12 @@ import RealityKit
 public extension MeshDescriptor {
     init(trivialMesh mesh: TrivialMesh<SimpleVertex>) {
         self = MeshDescriptor()
-        // TODO: We're ignoring normals and texture coordinates for now.
+        // TODO: We're ignoring texture coordinates for now.
         assert(mesh.isValid)
         assert(!mesh.indices.isEmpty)
         assert(mesh.indices.count < UInt32.max)
         positions = MeshBuffers.Positions(mesh.vertices.map(\.position))
+        normals = MeshBuffers.Normals(mesh.vertices.map(\.normal))
         primitives = .triangles(mesh.indices.map { UInt32($0) })
     }
 }
