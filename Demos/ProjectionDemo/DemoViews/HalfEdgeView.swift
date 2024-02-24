cfd84fa2ea4574ab8f188fd6c5c05db74da5ef64
Jonathan Wight <jwight@mac.com>
2024-02-24 09:45:37 -0800

    WIP

--- Demos/ProjectionDemo/DemoViews/HalfEdgeView.swift
+++ Demos/ProjectionDemo/DemoViews/HalfEdgeView.swift
@@ -1,27 +1,59 @@
 import SwiftUI
 import GeometryX
 import Projection
+import simd
+import CoreGraphicsSupport
 
 struct HalfEdgeView: View {
 
     var mesh: HalfEdgeMesh = .demo()
 
-
+    @State
+    var camera = Camera(transform: .translation([0, 0, -5]), target: [0, 0, 0], projection: .perspective(.init(fovy: .degrees(90), zClip: 0.01 ... 1000.0)))
 
     var body: some View {
-        SoftwareRendererView { _, _, context3D in
-            var rasterizer = context3D.rasterizer
-
-            for polygon in mesh.polygons {
-                rasterizer.submit(polygon: polygon.vertices, with: .color(.green))
+        GeometryReader { proxy in
+            Canvas { context, size in
+                var projection = Projection3D(size: size)
+                projection.viewTransform = camera.transform.matrix.inverse
+                projection.projectionTransform = camera.projection.matrix(viewSize: .init(size))
+                projection.clipTransform = simd_float4x4(scale: [Float(size.width) / 2, Float(size.height) / 2, 1])
+                context.draw3DLayer(projection: projection) { context, context3D in
+                    var rasterizer = context3D.rasterizer
+                    for polygon in mesh.polygons {
+                        rasterizer.submit(polygon: polygon.vertices, with: .color(.green))
+                    }
+                    rasterizer.rasterize()
+                }
             }
-            rasterizer.rasterize()
-        }
-        .onSpatialTap { location in
-            print(location)
+            .onSpatialTap { location in
+                let size = proxy.size
+                var projection = Projection3D(size: size)
+                projection.viewTransform = camera.transform.matrix.inverse
+                projection.projectionTransform = camera.projection.matrix(viewSize: .init(size))
+                projection.clipTransform = simd_float4x4(scale: [Float(size.width) / 2, Float(size.height) / 2, 1])
+
+                var location = location
+                location.x -= size.width / 2
+                location.y -= size.height / 2
+
+
+//                print(location)
+                mesh.polygons.forEach { polygon in
+                    let points = polygon.vertices.map { projection.project($0)}
+                    let path = Path(vertices: points, closed: true)
+                    print(points)
+                    if path.contains(location) {
+                        print("HIT")
+                    }
+                }
+            }
         }
     }
 }
+
+
+
 
 extension View {
     func onSpatialTap(count: Int = 1, coordinateSpace: some CoordinateSpaceProtocol = .local, handler: @escaping (CGPoint) -> Void) -> some View {
@@ -30,7 +62,6 @@ extension View {
         }))
     }
 }
-
 
 extension HalfEdgeMesh {
     static func demo() -> HalfEdgeMesh {
